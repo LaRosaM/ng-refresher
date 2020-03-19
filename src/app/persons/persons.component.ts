83d45580608ab7d349ea15bb27096b8e0c23c248
LaRosaM <jose.larosa6@unmsm.edu.pe>
2020-03-19 11:08:42 -0500

    Avance del tutorial

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -9,16 +9,19 @@ import { Subscription } from 'rxjs';
 
 export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[];
+  isFetching = false;
   private personListSubs: Subscription;
   constructor(private prsService: PersonsService) {
 
   }
 
   ngOnInit() {
-    this.personList = this.prsService.persons;
     this.personListSubs = this.prsService.personsChanged.subscribe(persons => {
       this.personList = persons;
+      this.isFetching = false;
     });
+    this.isFetching = true;
+    this.prsService.fetchPersons();
   }
 
   onRemovePerson(personName: string) {
